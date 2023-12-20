449328e712f7466537f3f212a35e7ce8c4f86958
Trezv1j <ruslan200508@gmail.com>
2023-12-20 19:26:58 +0200

    skolos

---
+++ PD4/js/main.js
@@ -0,0 +1,11 @@
+let petName = "Minotaur";
+let petAge = 16;
+
+console.log(petName , petAge);
+
+petName = "Prometheus"
+petAge = 60;
+
+const buildingAdress = "Trinapolio g. 3"
+
+console.log(buildingAdress);
